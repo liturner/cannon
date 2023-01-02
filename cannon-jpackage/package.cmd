90f0bfb0114e5261c6eecf3a5f34f44358086a13
liturner <turnertech@outlook.de>
2023-01-02 17:38:36 +0100

    Switch to JPackage instead

--- cannon-jpackage/package.cmd
+++ cannon-jpackage/package.cmd
@@ -1,2 +1,2 @@
 REM https://docs.oracle.com/en/java/javase/14/docs/specs/man/jpackage.html
-jpackage -n Cannon -p cannon-jlink/target/dependency -m de.turnertech.cannon/de.turnertech.cannon.Application --app-version 1.0 -d cannon-jpackage --vendor TurnerTech --icon cannon-jpackage/tt-logo.ico --license-file LICENSE.txt --win-shortcut --win-menu --win-menu-group TurnerTech
+jpackage -n Cannon -p cannon-jlink/target/dependency -m de.turnertech.cannon/de.turnertech.cannon.Application -d cannon-jpackage --vendor TurnerTech --icon cannon-jpackage/tt-logo.ico --license-file LICENSE.txt --win-shortcut --win-menu
